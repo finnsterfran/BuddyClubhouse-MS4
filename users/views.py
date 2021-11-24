b91b77c3fcf38d82e0838d064d3570c3fbd04e56
finnsterfran <j.frannie.yip@gmail.com>
2021-11-24 15:52:24 +0000

    add views for user registration, user login and logout, user profile page and account page, user account edit page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,132 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Profile
+from .forms import CustomUserCreationForm, ProfileForm
 
-# Create your views here.
+
+def userLogin(request):
+    """
+    For user login, redirecting user to 
+    make an account if user doesn't exist in the database
+    """
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.post['username']
+        password = request.post['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not seem to exist in our database')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome back {user}!')
+            return redirect('account')
+
+        else: 
+            messages.error(request, 'Uhm... username or/and password seems to be incorrect!')
+    return render(request, 'users/login_register.html')
+
+
+def userLogout(request):
+    """
+    Log the use out
+    """
+    logout(request)
+    messages.info(request, 'User is no longer logged in')
+    return redirect('login')
+
+
+def userRegister(request):
+    """
+    For user registration page
+    """
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'You have successfully created a new user account')
+
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('edit_account')
+        else:
+            messages.error(
+                request, 'Sorry, something went wrong with the registration')
+    context = {
+        'page': page,
+        'form': form,
+    }
+    return redirect(request, 'user/login_registration.html', context)
+
 
 def profiles(request):
+    """
+    Display all user profiles
+    """
+    profiles = Profile.objects.all()
+
+    context = {
+        'profiles': profiles
+    }
     return render(request, 'users/profiles.html')
+
+
+def profile(request, pk):
+    """
+    Display individual user profile
+    """
+    profile = Profile.objects.get(id=pk)
+
+    context = {
+        'profile': profile,
+    }
+    return render(request, 'users/profile.html', context)
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    """
+    User's ACCOUNT view
+    """
+    profile = request.user.profile
+    
+    context = {
+        'profile': profile
+    }
+    return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def edit_account(request):
+    """
+    User's ACCOUNT edit view, user can change profile 
+    and see all account related information
+    """
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request, 'Profile update successful!')
+        return redirect('account')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/profile_form.html', context)
