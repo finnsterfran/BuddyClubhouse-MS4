fb487e3b69131a5dbcbc065822b214b58dc651d3
finnsterfran <j.frannie.yip@gmail.com>
2021-11-24 10:08:36 +0000

    create about.html. add image, wording and css styling

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
-    """A view to return the home page"""
+    """
+    A view to return the HOME page
+    """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """
+    A view to render the ABOUT page
+    """
+    return render(request, 'home/about.html')
