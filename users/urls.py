d3979baa162e20d7316f7d256a3fe8cbd957aa6c
finnsterfran <j.frannie.yip@gmail.com>
2021-11-24 15:58:07 +0000

    add urls to urls.py for users app. create placeholder templates - account, login-register, profile_form, profile, profiles

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
+    path('login/', views.userLogin, name='login'),
+    path('logout/', views.userLogout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/<str:pk>/', views.profile, name='profile'),
+    path('account/', views.account, name='account'),
+    path('edit_account/', views.edit_account, name='edit_account'),
 ]
