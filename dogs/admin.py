98cb51e7d37ab71c8c920af5516d7bfa7db266f8
finnsterfran <j.frannie.yip@gmail.com>
2021-11-25 21:17:02 +0000

    create models Dogs, Breed, Job. Run migration. Register models and list display in admin.py

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Dog, Breed, Job
 
-# Register your models here.
+
+class DogAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'breed',
+        'resided_since',
+        'age',
+        'gender',
+        'image',
+        'story',
+    )
+    ordering = ('name')
+
+
+class BreedAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'breed_name',
+    )
+
+
+class JobAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+    )
+
+
+admin.site.register(Dog, DogAdmin)
+admin.site.register(Breed, BreedAdmin)
+admin.site.register(Job, JobAdmin)
--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,44 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+    
+    GENDER_TYPE = (
+        ('Female', 'female'),
+        ('Male', 'male'),
+    )
+
+    name = models.CharField(max_length=50)
+    breed = models.ForeignKey('Breed', on_delete=models.CASCADE, null=True)
+    age = models.IntegerField()
+    resided_since = models.DateField()
+    gender = models.CharField(max_length=6, choices=GENDER_TYPE)
+    job = models.ManyToManyField('Job', blank=True)
+    image = models.ImageField(upload_to='dogs/', null=True, blank=True)
+    story = models.TextField(max_length=2000, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Breed(models.Model):
+
+    breed_name = models.CharField(max_length=20)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.breed_name)
+
+
+class Job(models.Model):
+
+    title = models.CharField(max_length=200)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.title)
