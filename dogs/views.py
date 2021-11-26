14db06f272ad162b87c995a573e498ea1173906f
finnsterfran <j.frannie.yip@gmail.com>
2021-11-26 08:41:58 +0000

    add more dogs and images to the database. create view to render dogs.html and and dog_profile.html

--- dogs/views.py
+++ dogs/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def dogs(request):
-    return render(request, 'dogs/dogs.html')
+    """
+    display all dogs in the database
+    """
+    dogs = Dog.objects.all()
+
+    context = {
+        'dogs': dogs,
+    }
+    return render(request, 'dogs/dogs.html', context)
+
+
+def dog_profile(request, pk):
+    """
+    display individual dog's profile
+    """
+    dogObj = Dog.objects.get(id=pk)
+
+    context = {
+        'dog_profile': dogObj,
+    }
+    return render(request, 'dogs/dog_profile.html', context)
