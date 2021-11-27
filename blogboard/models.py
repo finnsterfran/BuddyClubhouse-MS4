7dde5a687304341e0de0448387b0ede2c4329df6
finnsterfran <j.frannie.yip@gmail.com>
2021-11-27 21:10:11 +0000

    create Blog model in models.py

--- blogboard/models.py
+++ blogboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import Profile
+from dogs.models import Dog 
+import uuid
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    username = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                                 null=True, blank=True)
+    name_of_buddy = models.ForeignKey(Dog, on_delete=models.CASCADE,
+                                      null=True)
+    date_of_entry = models.DateTimeField(auto_now_add=True)
+    featured_image = models.ImageField(upload_to='blogs/',
+                                       null=True, blank=True)
+    blog_entry = models.TextField(max_length=3000, null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    
+    def __str__(self):
+        return str(self.title)
