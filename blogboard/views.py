7cad3386c7852c3645311c9366d176478f4d6b93
finnsterfran <j.frannie.yip@gmail.com>
2021-11-27 21:27:09 +0000

    create view for write_blog, create write_blog.html

--- blogboard/views.py
+++ blogboard/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Blog
+from .forms import BlogForm
 
 
 def blogs(request):
     """
     View to see all the blogpost entries on the blogboard
     """
-    return render(request, 'blogboard/blogs.html')
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blogboard/blogs.html', context)
+
+
+
+
+@login_required(login_url='login')
+def write_blog(request):
+    """
+    View to create and submit a new blog post entry
+    """
+    profile = request.user.profile
+    form = BlogForm()
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.username = profile
+            blog.save()
+
+            messages.success(request, 'You have submitted a new blog entry!')
+            return redirect('blogs')
+
+    context = {
+        'form': form,
+        'profile': profile,
+    }
+    return render(request, 'blogboard/write_blog.html', context)
