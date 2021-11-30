58d19ab8180752ec1e86c0273796125c25138551
finnsterfran <j.frannie.yip@gmail.com>
2021-11-30 18:04:41 +0000

    create Donation model in models.py. migrate. register model in admin.py

--- contribution/models.py
+++ contribution/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Donation(models.Model):
+    """
+    Model for choices of donation that user can make
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000)
+    short_info = models.CharField(max_length=255, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
