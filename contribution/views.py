52cd04b0cf27c8bf4de0a91bd158c92220f2b2f7
finnsterfran <j.frannie.yip@gmail.com>
2021-12-01 08:50:28 +0000

    add html to contribution.html. set view for rendering donation options.

--- contribution/views.py
+++ contribution/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from .models import Donation
 
 
 def contribution(request):
     """ A view to show donation plans available """
-    return render(request, 'contribution/contribution.html')
+    products = Donation.objects.all().order_by('price')
+    context = {
+        'products': products
+    }
+    return render(request, 'contribution/contribution.html', context)
 
