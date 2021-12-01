fcc69e6bc6919eb12c107359125ec6b704506bd9
finnsterfran <j.frannie.yip@gmail.com>
2021-12-01 09:57:29 +0000

    finish creating cart.html. add adjust_cart view to views.py

--- contribution/urls.py
+++ contribution/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.contribution, name='contribution'),
+    path('cart/', views.cart, name='cart'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- contribution/views.py
+++ contribution/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
 from django.contrib import messages
 from .models import Donation
@@ -12,3 +12,23 @@ def contribution(request):
     }
     return render(request, 'contribution/contribution.html', context)
 
+
+def cart(request):
+    """ A view to show shopping cart """
+    return render(request, 'contribution/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add quantity of selection to the cart """
+    product = get_object_or_404(Donation, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+    
+    request.session['cart'] = cart
+    return redirect(redirect_url)
