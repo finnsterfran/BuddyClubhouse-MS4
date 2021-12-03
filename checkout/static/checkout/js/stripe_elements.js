4ada64b01ce5837f27347f4eceb8370409cf7963
finnsterfran <j.frannie.yip@gmail.com>
2021-12-03 15:01:16 +0000

    add styling to checkout.css. add stripe script to stripe_elements.js

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,102 @@
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+card.addEventListener('change', function(event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+        <span role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+    `;
+    $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    card.update({'disabled': true})
+    $('#submit-button').attr('disabled, true');
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.first_name.value).concat(' ', $.trim(form.last_name.value)),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.address_line_1.value),
+                        line2: $.trim(form.address_line_2.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.first_name.value).concat(' ', $.trim(form.last_name.value)),
+                address: {
+                    line1: $.trim(form.address_line_1.value),
+                    line2: $.trim(form.address_line_2.value),
+                    postal_code: $.trim(form.postal_code.value)
+                }
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span role="alert">
+                        <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>
+                `;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({'disabled': false})
+                $('#submit-button').attr('disabled, false');
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit()
+                }
+            }
+        });
+    }).fail(function () {
+        location.reload();
+    })
+});
+
