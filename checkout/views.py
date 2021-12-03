eedc02a82a5492f16836025dcf1e3bc5113aed36
finnsterfran <j.frannie.yip@gmail.com>
2021-12-03 15:06:55 +0000

    create checkout_success.html to show details about the order that was successfully handled. update views to render checkout, checkout_success

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,141 @@
-from django.shortcuts import render
+import json
+import stripe
+from django.shortcuts import (render, redirect, reverse,
+                              get_object_or_404, HttpResponse)
+from django.contrib import messages
+from django.conf import settings
+
+from django.views.decorators.http import require_POST
+from users.models import Profile
+from users.forms import ProfileForm
+from contribution.models import Donation
+from contribution.contexts import cart_contents
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot \
+             be processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
-    return render(request, 'checkout/checkout.html')
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+        form_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'address_line_1': request.POST['address_line_1'],
+            'address_line_2': request.POST['address_line_2'],
+            'postcode': request.POST['postcode'],
+        }
+        
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            # prevent multiple save being made, commit=False
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
+            for item_id, item_data in cart.items():
+                product = Donation.objects.get(id=item_id)
+                if isinstance(item_data, int):
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+
+            request.session['save-info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success',
+                            args=[order.order_number]))
+        else:
+            messages.error(request, 'There was something wrong with your form. \
+                Please double check your information.')
+
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request,
+                           "There is nothing in your bag at the moment")
+            return redirect(reverse('products'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        if request.user.is_authenticated:
+            try:
+                profile = request.user.profile
+                order_form = OrderForm(initial={
+                    'first_name': profile.first_name,
+                    'last_name': profile.last_name,
+                    'email': profile.email,
+                    'address_line_1': profile.address_line_1,
+                    'address_line_2': profile.address_line_2,
+                    'postcode': profile.postcode,
+                })
+            except Profile.DoesNotExist():
+                order_form = OrderForm()
+        
+        else:
+            order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your information?')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    handle successful checkouts
+    """
+
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+            email will be sent to {order.email}.')
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
