446c86791145d67472f374a38c0646075a4ebd2e
finnsterfran <j.frannie.yip@gmail.com>
2021-12-08 16:29:56 +0000

    create users - test.models

--- users/test_models.py
+++ users/test_models.py
@@ -1,1 +1,22 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
+
+
+class TestProfileModel(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='looloo', password='yo!4password')
+
+    def test_can_create_profile(self):
+        profile = Profile(
+            user=self.user,
+            first_name='Lola',
+            last_name='Montez',
+            username='looloo',
+            email='looloo@email.com',
+            address_line_1='South Beach Street 99',
+            address_line_2='',
+            postal_code='23459RR',
+        )
+        self.assertEqual(profile.user.username, 'looloo')
+        self.assertEqual('looloo', str(profile))
