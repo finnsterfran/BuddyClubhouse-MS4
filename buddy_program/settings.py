e8e44d15f77d3c0485d15132dd944c6ba160c97a
finnsterfran <j.frannie.yip@gmail.com>
2021-12-10 09:35:19 +0000

    set up heroku deployment

--- buddy_program/settings.py
+++ buddy_program/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -141,7 +142,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
