2f30eddc635291d6f4318bb25a14e2681de61b03
finnsterfran <j.frannie.yip@gmail.com>
2021-12-11 09:09:39 +0000

    change some email settin - registration bug

--- buddy_program/settings.py
+++ buddy_program/settings.py
@@ -107,9 +107,9 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', '')
     EMAIL_HOST = 'smtp.aol.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_SSL = False
-    EMAIL_USE_TLS = True
+    EMAIL_PORT = 465
+    EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = False
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
     EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD', '')
 
