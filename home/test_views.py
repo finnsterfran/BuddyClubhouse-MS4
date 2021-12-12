a3d4708f07b80cc49ace23b83f7a0f8a8ec8bc67
finnsterfran <j.frannie.yip@gmail.com>
2021-12-12 07:16:40 +0000

    add testing for views in home app

---
+++ home/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+class TestHomeViews(TestCase):
+
+    def test_get_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_about_page(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_contactus_page(self):
+        response = self.client.get('/contact_us/')
+        self.assertEqual(response.status_code, 200)
