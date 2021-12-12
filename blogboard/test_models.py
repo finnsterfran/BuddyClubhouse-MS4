743e0c79a213eee7fff01a8dcecf6681c528039e
finnsterfran <j.frannie.yip@gmail.com>
2021-12-12 06:48:32 +0000

    add str method testing for blogboard/test_models.py

---
+++ blogboard/test_models.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from .models import Blog
+
+
+class TestModels(TestCase):
+    def test_blog_str_method_returns_name(self):
+        blog = Blog.objects.create(title='Test this')
+        self.assertEqual(str(blog), 'Test this')
