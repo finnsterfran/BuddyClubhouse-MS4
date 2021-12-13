b5c2c1d5b9a8f5a31c81e826e03cb39a1c3ca676
finnsterfran <j.frannie.yip@gmail.com>
2021-12-13 08:38:17 +0000

    fix field for event_type in models.py. adjust accounts.html

--- events/models.py
+++ events/models.py
@@ -4,11 +4,11 @@ from dogs.models import Dog
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
-    event_type = models.OneToOneField('Event_Type',
+    event_type = models.ForeignKey('Event_Type',
                                       on_delete=models.CASCADE,
                                       null=True)
     buddy_name = models.ManyToManyField(Dog, blank=True)
-    date_of_event = models.DateField()
+    date_of_event = models.DateField(help_text='YYYY/MM/DD')
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
 
