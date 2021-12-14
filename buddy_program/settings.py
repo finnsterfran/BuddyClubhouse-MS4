f779846065986ccd8db1c1ee8126c9fb7348903e
finnsterfran <j.frannie.yip@gmail.com>
2021-12-14 11:55:33 +0000

    change email setting.

--- buddy_program/settings.py
+++ buddy_program/settings.py
@@ -113,7 +113,7 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST = 'smtp.aol.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
